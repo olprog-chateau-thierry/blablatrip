75bef1a378449a458b7138cf3a7ad2044511e687
Samuel MICHAUX <samuel.michaux@ik.me>
2023-09-04 16:51:07 +0200

    un peu de design dans l'admin

---
+++ src/main/java/com/formation/blablatrip/controllers/admin/AdminController.java
@@ -0,0 +1,15 @@
+package com.formation.blablatrip.controllers.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController extends AdminBaseController{
+
+    @GetMapping("")
+    public String index(Model model) {
+        model.addAttribute("content", "accueil.html");
+        return "admin/index";
+    }
+}
